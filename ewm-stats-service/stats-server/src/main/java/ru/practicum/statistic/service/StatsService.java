9d2aef0155e1f1177dc2e217889eff00655e23e2
unknown <eleonora.brueva@yandex.com>
2026-06-25 00:32:21 +0300

    Fix checkstyle: add empty line after interface declaration

--- ewm-stats-service/stats-server/src/main/java/ru/practicum/statistic/service/StatsService.java
+++ ewm-stats-service/stats-server/src/main/java/ru/practicum/statistic/service/StatsService.java
@@ -6,6 +6,8 @@ import ru.practicum.statistic.dto.ViewStatsDto;
 import java.util.List;
 
 public interface StatsService {
+
     void saveHit(EndpointHitDto dto);
+
     List<ViewStatsDto> getStats(String start, String end, List<String> uris, Boolean unique);
 }
