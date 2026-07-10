8872d6eba37797955f2264561a23601c9246dffe
unknown <eleonora.brueva@yandex.com>
2026-07-11 00:00:50 +0300

    Добавлена обработка MissingServletRequestParameterException

--- ewm-stats-service/stats-server/src/main/java/ru/practicum/statistic/exception/ErrorHandler.java
+++ ewm-stats-service/stats-server/src/main/java/ru/practicum/statistic/exception/ErrorHandler.java
@@ -1,6 +1,7 @@
 package ru.practicum.statistic.exception;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -14,23 +15,22 @@ public class ErrorHandler {
     @ExceptionHandler(BadRequestException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public Map<String, Object> handleBadRequest(BadRequestException e) {
-        return Map.of(
-                "timestamp", LocalDateTime.now(),
-                "status", HttpStatus.BAD_REQUEST.value(),
-                "error", "Bad Request",
-                "message", e.getMessage(),
-                "path", "/stats"
-        );
+        return buildErrorResponse(HttpStatus.BAD_REQUEST, e.getMessage());
     }
 
-    @ExceptionHandler(Exception.class)
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public Map<String, Object> handleException(Exception e) {
+    @ExceptionHandler(MissingServletRequestParameterException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, Object> handleMissingParams(MissingServletRequestParameterException e) {
+        String message = "Required request parameter '" + e.getParameterName() + "' is not present";
+        return buildErrorResponse(HttpStatus.BAD_REQUEST, message);
+    }
+
+    private Map<String, Object> buildErrorResponse(HttpStatus status, String message) {
         return Map.of(
-                "timestamp", LocalDateTime.now(),
-                "status", HttpStatus.INTERNAL_SERVER_ERROR.value(),
-                "error", "Internal Server Error",
-                "message", e.getMessage(),
+                "timestamp", LocalDateTime.now().toString(),
+                "status", status.value(),
+                "error", status.getReasonPhrase(),
+                "message", message,
                 "path", "/stats"
         );
     }
